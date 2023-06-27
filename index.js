adbcdf78bd4bb7f8d02f135d7c0458d1bbb31177
Teddy Pascual <teddylayson2000@gmail.com>
2023-06-27 19:28:47 +0800

    documented files

--- index.js
+++ index.js
@@ -7,55 +7,6 @@ form.addEventListener("submit", (e) => {
   form.classList.add("was-validated");
 });
 */
-function radioChange(val) {
-  const form = document.getElementById("credentials_inp");
-  const formText = document.getElementById("credentials-label");
-  if (val === "farmer") {
-    formText.innerText = `Upload RSBSA: `;
-  } else {
-    formText.innerText = "Upload Valid ID: ";
-  }
-}
-
-function signup() {
-  window.location.assign("SignComplete.html");
-  alert("Hello");
-}
-
-function passCheck() {
-  const pass = document.getElementById("password");
-  const conf_pass = document.getElementById("conf-password");
-  const btn = document.getElementById("form-submit");
-  if (pass.value === conf_pass.value) {
-    pass.classList.remove("is-invalid");
-    conf_pass.classList.remove("is-invalid");
-    pass.classList.add("is-valid");
-    conf_pass.classList.add("is-valid");
-    btn.disabled = false;
-  } else {
-    pass.classList.remove("is-valid");
-    conf_pass.classList.remove("is-valid");
-    pass.classList.add("is-invalid");
-    conf_pass.classList.add("is-invalid");
-    btn.disabled = true;
-  }
-}
-
-function phoneCheck() {
-  const btn = document.getElementById("form-submit");
-  const num = document.getElementById("phone-number");
-  let start = num.value.slice(0, 2);
-
-  if (num.value.length !== 11 || start !== "09") {
-    num.classList.remove("is-valid");
-    num.classList.add("is-invalid");
-    btn.disabled = true;
-  } else {
-    num.classList.remove("is-invalid");
-    num.classList.add("is-valid");
-    btn.disabled = false;
-  }
-}
 
 function edit() {
   preventDefault();
@@ -63,7 +14,7 @@ function edit() {
   alert(val);
   btn.disabled = false;
 }
-
+// Change Img on file upload and enables save button on the image container
 let img = document.getElementById("profile-pic");
 let input = document.getElementById("change-prof");
 let saveImg = document.getElementById("save-img-btn");
@@ -73,14 +24,12 @@ input.addEventListener("change", () => {
   saveImg.disabled = false;
 });
 
-saveImg.addEventListener("onclick", (e) => {
-  e.preventDefault();
-});
-
+// Disable the text input on the different form control on the form container
 function ror(val) {
   document.getElementById(`${val}`).disabled = false;
 }
 
+// enable save btn on form cotnainer
 function boom() {
   mySaveBtn.disabled = false;
 }
---
+++ signUp.js
@@ -0,0 +1,59 @@
+// Change the image of the sign up to the the uploaded pic
+let signUpImg = document.getElementById("signUpVect");
+let signUpInp = document.getElementById("pic-sign-up");
+signUpInp.addEventListener("change", () => {
+  signUpImg.src = URL.createObjectURL(signUpInp.files[0]);
+});
+
+// changes radio label to the specific type of file they need to upload
+function radioChange(val) {
+  const form = document.getElementById("credentials_inp");
+  const formText = document.getElementById("credentials-label");
+  if (val === "farmer") {
+    formText.innerText = `Upload RSBSA: `;
+  } else {
+    formText.innerText = "Upload Valid ID: ";
+  }
+}
+// relocate the user to the SignComplete after signing up
+function signup() {
+  window.location.assign("SignComplete.html");
+  alert("Hello");
+}
+
+// checks if password are the same for both the inputs
+function passCheck() {
+  const pass = document.getElementById("password");
+  const conf_pass = document.getElementById("conf-password");
+  const btn = document.getElementById("form-submit");
+  if (pass.value === conf_pass.value) {
+    pass.classList.remove("is-invalid");
+    conf_pass.classList.remove("is-invalid");
+    pass.classList.add("is-valid");
+    conf_pass.classList.add("is-valid");
+    btn.disabled = false;
+  } else {
+    pass.classList.remove("is-valid");
+    conf_pass.classList.remove("is-valid");
+    pass.classList.add("is-invalid");
+    conf_pass.classList.add("is-invalid");
+    btn.disabled = true;
+  }
+}
+
+// checks if it is a valid PH phone number
+function phoneCheck() {
+  const btn = document.getElementById("form-submit");
+  const num = document.getElementById("phone-number");
+  let start = num.value.slice(0, 2);
+
+  if (num.value.length !== 11 || start !== "09") {
+    num.classList.remove("is-valid");
+    num.classList.add("is-invalid");
+    btn.disabled = true;
+  } else {
+    num.classList.remove("is-invalid");
+    num.classList.add("is-valid");
+    btn.disabled = false;
+  }
+}
