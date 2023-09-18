660ab2f357edb488dcd62021abaf2fe784473a75
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-18 18:38:31 +0800

    updated web and mobile timer

--- timer.js
+++ timer.js
@@ -38,14 +38,49 @@ function update() {
       Seconds = 59;
     }
   }
-
-  // if (Seconds > 0) {
-  //   Seconds--;
-  //   if (Seconds < 10) {
-  //     displaySeconds.innerHTML = `0${Seconds}`;
-  //   } else {
-  //     displaySeconds.innerHTML = Seconds;
-  //   }
-  // }
 }
 myInterval = setInterval(update, 1000);
+
+//Web Version
+let displayHours2 = document.getElementById("displayHours2");
+let displayMinutes2 = document.getElementById("displayMinutes2");
+let displaySeconds2 = document.getElementById("displaySeconds2");
+
+let Hours2 = 12;
+let Minutes2 = 0;
+let Seconds2 = 0;
+function update2() {
+  if (Hours2 >= 10) {
+    displayHours2.innerHTML = Hours2;
+  } else {
+    displayHours2.innerHTML = `0${Hours2}`;
+  }
+  if (Minutes2 >= 10) {
+    displayMinutes2.innerHTML = Minutes2;
+  } else {
+    displayMinutes2.innerHTML = `0${Minutes2}`;
+  }
+  if (Seconds2 >= 10) {
+    displaySeconds2.innerHTML = Seconds2;
+  } else {
+    displaySeconds2.innerHTML = `0${Seconds2}`;
+  }
+  if (Hours2 == 0 && Minutes2 == 0 && Seconds2 == 0) {
+    alert("Timer is Up!");
+    clearInterval(myInterval);
+  } else {
+    if (Minutes2 == 0 && Seconds2 == 0) {
+      Hours2--;
+      console.log(Hours2);
+      Minutes2 = 59;
+      Seconds2 = 59;
+    } else if (Seconds2 > 0) {
+      Seconds2--;
+    } else if (Seconds2 == 0) {
+      Minutes2--;
+
+      Seconds2 = 59;
+    }
+  }
+}
+myInterval2 = setInterval(update2, 1000);
