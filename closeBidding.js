7db64e22ce27a487b789a321992d43700e726cb8
Teddy Pascual <teddylayson2000@gmail.com>
2023-10-26 19:56:38 +0800

    improve bidding owner controls

---
+++ closeBidding.js
@@ -0,0 +1,5 @@
+const updBtn = document.getElementById("updateBbpBtn");
+const updForm = document.getElementById("updForm");
+updBtn.addEventListener("click", function () {
+  updForm.submit();
+});
